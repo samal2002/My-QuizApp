469ed5fc2195af880e11c20df9d231f70fc50d2f
samal2002 <gouravranjansamal@gmail.com>
2025-01-10 08:34:26 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        Question: 'What is Redux?',
+        Option1: 'Redux is State Management System',
+        Option2: 'Redux is State',
+        Option3: 'Redux is Data Type',
+        Option4: 'Redux is a part of HTML'
+    },
+    {
+        Question: 'What is the primary use of Redux?',
+        Option1: 'To manage and centralize application state',
+        Option2: 'To create CSS styles',
+        Option3: 'To handle API requests',
+        Option4: 'To manage HTML templates'
+    },
+    {
+        Question: 'What are the core principles of Redux?',
+        Option1: 'Single source of truth, state is read-only, changes are made with pure functions',
+        Option2: 'State is shared, changes are made with impure functions',
+        Option3: 'State is distributed, changes are automatic',
+        Option4: 'None of the above'
+    }
+];
